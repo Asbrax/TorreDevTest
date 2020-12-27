1a7620a68e9274cd21c2f61e39ffe38f70a015b5
Jorge <asbrax.go@gmail.com>
2020-12-26 23:31:42 -0600

    setup project basics

---
+++ src/app/components/genome/genome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genome',
+  templateUrl: './genome.component.html',
+  styleUrls: ['./genome.component.less']
+})
+export class GenomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
